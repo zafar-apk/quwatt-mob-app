89dfe66d6d19b2d3563f8a6f0786f7a03883df7a
zafar.abdulloev <zafar.abdulloev@umbrellait.com>
2023-12-11 10:01:18 +0500

    fixed bug with activity

--- composeApp/src/androidMain/kotlin/tj/yakroh/yakrohapp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/tj/yakroh/yakrohapp/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import moe.tlaster.precompose.lifecycle.PreComposeActivity
+import moe.tlaster.precompose.lifecycle.setContent
 
-class MainActivity : ComponentActivity() {
+class MainActivity : PreComposeActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             MainView()
         }
